29afa6f51c69aedf5fb33ee625b9a791f404c8a5
da4do0 <79230118+da4do0@users.noreply.github.com>
2022-09-08 12:17:21 +0200

    adattato il sito ad html
    tolto tutti i php, adattato css ai vari dispositivi.

--- php/connessione-admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "127.0.0.1";
-    $user = "root";
-    $password = "";
-    $database = "portfolio personale";
-    $connessione = new mysqli ($host, $user, $password, $database);
-?>
